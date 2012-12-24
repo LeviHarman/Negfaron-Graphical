2ed4178267b5cf2379edff8e9947fd12805ffcc9
Levi Harman <serpentinexcubed@gmail.com>
2012-12-24 08:18:53 -0600

    seperated logic into three functions for maintainability

--- move_hero.h
+++ move_hero.h
@@ -1,89 +1,111 @@
+/*draws 3 frame animations.
+variables
+tileset, tile width, Entity being animated,  1-3
+column for first, second, and third frames. 4-6
+row for first,second, and third frames. 7-9
+animation height in tiles, animation width in tiles. 10,11
+radj, and cadj are for adjusting the center of animation. makes function tweakable.
+*/
+
+Entity three_frame_animation(ALLEGRO_BITMAP* tileset, int tw, Entity hero,int ci1,int ci2, int ci3, int ri1, int ri2, int ri3,int aheight,int awidth,int radj, int cadj) {
+	if(hero.sta_frame<=9) {
+		al_draw_bitmap_region(tileset, (ri1)*tw, (ci1)*tw, tw*awidth, tw*aheight, (16+radj)*tw, (16+cadj)*tw, 0);
+	}
+	else if(hero.sta_frame<=19) {
+		al_draw_bitmap_region(tileset, (ri2)*tw, (ci2)*tw, tw*awidth, tw*aheight, (16+radj)*tw, (16+cadj)*tw, 0);
+	}
+	else if(hero.sta_frame<=29) {
+		al_draw_bitmap_region(tileset, (ri3)*tw, (ci3)*tw, tw*awidth, tw*aheight, (16+radj)*tw, (16+cadj)*tw, 0);
+	}
+	else if (hero.sta_frame <= 30) {
+		al_draw_bitmap_region(tileset, (ri3)*tw, (ci3)*tw, tw*awidth, tw*aheight, (16+radj)*tw, (16+cadj)*tw, 0);
+		hero.is_swing_hoe = false;
+	}
+	return hero;
+}
+
+/*
+Draws the walking animation
+
+Variable description
+
+Entity being animated, tileset,map vector 1-3
+tile width, animation height in tiles, and animation width in tiles
+height, and width of map in tiles.
+ci1,ci2,ci3 column of the three animations
+ri1,ri2,ri3 row of the three animations
+
+*/
+
+Entity walk_animation (Entity hero, ALLEGRO_BITMAP* tileset, vector<vector<Tile>> mv, int tw, int aheight, int awidth, int height, int width, int ci1,int ci2,int ci3,int ri1, int ri2, int ri3) {
+	if(hero.move_animation==true) {
+		int frameintv=0;
+		int frameinth=0;
+		int hl=0;
+		int vl=0;
+		switch (hero.facing) {
+		case 'u':
+			vl = -1;
+			frameintv = hero.frame;
+			break;
+		case 'd' :
+			vl=1;
+			frameintv = hero.frame *-1;
+			break;
+		case 'l' :
+			hl=-1;
+			frameinth = hero.frame;
+			break;
+		case 'r':
+			hl=1;
+			frameinth = hero.frame *-1;
+			break;
+		}
+
+		for(int col=0;col<height;col++) {
+			for(int row=0;row<width;row++) {
+				al_draw_bitmap_region(tileset, mv[col][row].sx * tw, mv[col][row].sy * tw, tw, tw, (row*tw - hero.wloc*tw)+15*tw+frameinth, (col*tw - hero.hloc*tw)+15*tw+frameintv, 0);
+			}
+		}	
+
+		if (hero.frame<= 7) {
+			al_draw_bitmap_region(tileset, ri2*tw, ci2*tw, tw, tw, 15*tw, 15*tw, 0);
+		}
+		else if (hero.frame < 15) {
+			al_draw_bitmap_region(tileset, ri3*tw, ci3*tw, tw, tw, 15*tw, 15*tw, 0);
+		}
+		else if(hero.frame == 15) {
+			al_draw_bitmap_region(tileset, ri3*tw, ci3*tw, tw, tw, 15*tw, 15*tw, 0);
+			hero.move_animation = false;
+			hero.hloc+=vl;
+			hero.wloc+=hl;
+		}
+		hero.frame++;
+	}
+	else {
+		al_draw_bitmap_region(tileset, ri1 * tw, ci1 * tw, tw, tw, 15*tw, 15*tw, 0);
+	}
+
+	return hero;
+}
+
 Entity move_hero (Entity hero, ALLEGRO_BITMAP* tileset,vector<vector<Tile>> mv,int height,int width) {
+	int tw = 16;
+
 	if(hero.move_animation==false) {
 		if(hero.is_swing_hoe == true) {
 			switch (hero.facing) {
 			case 'u':
-				if(hero.sta_frame<=9) {
-					al_draw_bitmap_region(tileset, 0 * 16, 17 * 16, 16, 16, 15 * 16, 15 * 16, 0);
-					al_draw_bitmap_region(tileset, 0 * 16, 16 * 16, 16, 16, 15 * 16, 14 * 16, 0);
-				}
-				else if(hero.sta_frame<=19) {
-					al_draw_bitmap_region(tileset, 1 * 16, 17 * 16, 16, 16, 15 * 16, 15 * 16, 0);
-					al_draw_bitmap_region(tileset, 1 * 16, 16 * 16, 16, 16, 15 * 16, 14 * 16, 0);
-				}
-				else if(hero.sta_frame<=29) {
-					al_draw_bitmap_region(tileset, 2 * 16, 17 * 16, 16, 16, 15 * 16, 15 * 16, 0);
-					al_draw_bitmap_region(tileset, 2 * 16, 16 * 16, 16, 16, 15 * 16, 14 * 16, 0);
-				}
-				else if (hero.sta_frame <= 30) {
-					al_draw_bitmap_region(tileset, 2 * 16, 17 * 16, 16, 16, 15 * 16, 15 * 16, 0);
-					al_draw_bitmap_region(tileset, 2 * 16, 16 * 16, 16, 16, 15 * 16, 14 * 16, 0);
-					hero.is_swing_hoe = false;
-				}
+				hero = three_frame_animation(tileset,tw,hero,16,16,16,0,1,2,2,1,-1,-2);
 				break;
 			case 'd' :
-				if(hero.sta_frame<=9) {
-					al_draw_bitmap_region(tileset, 0 * 16, 13 * 16, 16, 16, 15 * 16, 14 * 16, 0);
-					al_draw_bitmap_region(tileset, 0 * 16, 14 * 16, 16, 16, 15 * 16, 15 * 16, 0);
-				}
-				else if(hero.sta_frame<=19) {
-					al_draw_bitmap_region(tileset, 1 * 16, 13 * 16, 16, 16, 15 * 16, 14 * 16, 0);
-					al_draw_bitmap_region(tileset, 1 * 16, 14 * 16, 16, 16, 15 * 16, 15 * 16, 0);
-				}
-				else if(hero.sta_frame<=29) {
-					al_draw_bitmap_region(tileset, 2 * 16, 13 * 16, 16, 16, 15 * 16, 14 * 16, 0);
-					al_draw_bitmap_region(tileset, 2 * 16, 14 * 16, 16, 16, 15 * 16, 15 * 16, 0);
-					al_draw_bitmap_region(tileset, 2 * 16, 15 * 16, 16, 16, 15 * 16, 16 * 16, 0);
-				}
-				else if (hero.sta_frame <= 30) {
-					al_draw_bitmap_region(tileset, 2 * 16, 13 * 16, 16, 16, 15 * 16, 14 * 16, 0);
-					al_draw_bitmap_region(tileset, 2 * 16, 14 * 16, 16, 16, 15 * 16, 15 * 16, 0);
-					al_draw_bitmap_region(tileset, 2 * 16, 15 * 16, 16, 16, 15 * 16, 16 * 16, 0);
-					hero.is_swing_hoe = false;
-				}
+				hero = three_frame_animation(tileset,tw,hero,13,13,13,0,1,2,3,1,-1,-2);
 				break;
 			case 'l':
-				if(hero.sta_frame<=9) {
-					al_draw_bitmap_region(tileset, 0 * 16, 18 * 16, 16, 16, 15 * 16, 14 * 16, 0);
-					al_draw_bitmap_region(tileset, 0 * 16, 19 * 16, 16, 16, 15 * 16, 15 * 16, 0);
-				}
-				else if(hero.sta_frame<=19) {
-					al_draw_bitmap_region(tileset, 1 * 16, 18 * 16, 16, 16, 14 * 16, 14 * 16, 0);
-					al_draw_bitmap_region(tileset, 1 * 16, 19 * 16, 16, 16, 14 * 16, 15 * 16, 0);
-					al_draw_bitmap_region(tileset, 2 * 16, 18 * 16, 16, 16, 15 * 16, 14 * 16, 0);
-					al_draw_bitmap_region(tileset, 2 * 16, 19 * 16, 16, 16, 15 * 16, 15 * 16, 0);
-				}
-				else if(hero.sta_frame<=29) {
-					al_draw_bitmap_region(tileset, 3 * 16, 19 * 16, 16, 16, 14 * 16, 15 * 16, 0);
-					al_draw_bitmap_region(tileset, 4 * 16, 19 * 16, 16, 16, 15 * 16, 15 * 16, 0);
-				}
-				else if (hero.sta_frame <= 30) {
-					al_draw_bitmap_region(tileset, 3 * 16, 19 * 16, 16, 16, 14 * 16, 15 * 16, 0);
-					al_draw_bitmap_region(tileset, 4 * 16, 19 * 16, 16, 16, 15 * 16, 15 * 16, 0);
-					hero.is_swing_hoe = false;
-				}
+				hero = three_frame_animation(tileset,tw,hero,18,18,18,0,2,4,2,2,-2,-2);
 				break;
 			case 'r':
-				if(hero.sta_frame<=9) {
-					al_draw_bitmap_region(tileset, 0 * 16, 20 * 16, 16, 16, 15 * 16, 14 * 16, 0);
-					al_draw_bitmap_region(tileset, 0 * 16, 21 * 16, 16, 16, 15 * 16, 15 * 16, 0);
-				}
-				else if(hero.sta_frame<=19) {
-					al_draw_bitmap_region(tileset, 1 * 16, 20 * 16, 16, 16, 15 * 16, 14 * 16, 0);
-					al_draw_bitmap_region(tileset, 1 * 16, 21 * 16, 16, 16, 15 * 16, 15 * 16, 0);
-					al_draw_bitmap_region(tileset, 2 * 16, 20 * 16, 16, 16, 16 * 16, 14 * 16, 0);
-					al_draw_bitmap_region(tileset, 2 * 16, 21 * 16, 16, 16, 16 * 16, 15 * 16, 0);
-				}
-				else if(hero.sta_frame<=29) {
-					al_draw_bitmap_region(tileset, 3 * 16, 21 * 16, 16, 16, 15 * 16, 15 * 16, 0);
-					al_draw_bitmap_region(tileset, 4 * 16, 21 * 16, 16, 16, 16 * 16, 15 * 16, 0);
-				}
-				else if (hero.sta_frame <= 30) {
-					al_draw_bitmap_region(tileset, 3 * 16, 21 * 16, 16, 16, 15 * 16, 15 * 16, 0);
-					al_draw_bitmap_region(tileset, 4 * 16, 21 * 16, 16, 16, 16 * 16, 15 * 16, 0);
-					hero.is_swing_hoe = false;
-				}
-				break;
+				hero = three_frame_animation(tileset,tw,hero,20,20,20,0,2,4,2,2,-1,-2);
 				break;
 			}
 
@@ -93,103 +115,16 @@ Entity move_hero (Entity hero, ALLEGRO_BITMAP* tileset,vector<vector<Tile>> mv,i
 	if(hero.is_swing_hoe == false) {
 		switch (hero.facing) {
 		case 'u':
-			if(hero.move_animation==true) {
-				for(int col=0;col<height;col++) {
-					for(int row=0;row<width;row++) {
-						al_draw_bitmap_region(tileset, mv[col][row].sx * 16, mv[col][row].sy * 16, 16, 16, (row*16 - hero.wloc*16)+15*16, (col*16 - hero.hloc*16)+15*16+hero.frame, 0);
-					}
-				}	
-				if (hero.frame<= 7) {
-					al_draw_bitmap_region(tileset, 1 * 16, 3 * 16, 16, 16, 15*16, 15*16, 0);
-				}
-				else if (hero.frame < 15) {
-					al_draw_bitmap_region(tileset, 2 * 16, 3 * 16, 16, 16, 15*16, 15*16, 0);
-				}
-				else if(hero.frame == 15) {
-					al_draw_bitmap_region(tileset, 2 * 16, 3 * 16, 16, 16, 15*16, 15*16, 0);
-					hero.move_animation = false;
-					hero.hloc-=1;
-				}
-				hero.frame++;
-
-
-			}
-			else {
-				al_draw_bitmap_region(tileset, 0 * 16, 3 * 16, 16, 16, 15*16, 15*16, 0);
-			}
+			hero = walk_animation(hero,tileset,mv,tw,1,1,height,width,3,3,3,0,1,2);
 			break;
 		case 'd':
-			if(hero.move_animation==true) {
-				for(int col=0;col<height;col++) {
-					for(int row=0;row<width;row++) {
-						al_draw_bitmap_region(tileset, mv[col][row].sx * 16, mv[col][row].sy * 16, 16, 16, (row*16 - hero.wloc*16)+15*16, (col*16 - hero.hloc*16)+15*16-hero.frame, 0);
-					}
-				}	
-				if(hero.frame <=7) {
-					al_draw_bitmap_region(tileset, 1 * 16, 2 * 16, 16, 16, 15*16, 15*16, 0);
-				}
-				else if(hero.frame <= 14) {
-					al_draw_bitmap_region(tileset, 2 * 16, 2 * 16, 16, 16, 15*16, 15*16, 0);
-				}
-				else if(hero.frame==15) {
-					al_draw_bitmap_region(tileset, 2 * 16, 2 * 16, 16, 16, 15*16, 15*16, 0);
-					hero.move_animation = false;
-					hero.hloc+=1;
-				}
-				hero.frame++;
-			}
-			else {
-				al_draw_bitmap_region(tileset, 0 * 16, 2 * 16, 16, 16, 15*16, 15*16, 0);
-			}
+			hero = walk_animation(hero,tileset,mv,tw,1,1,height,width,2,2,2,0,1,2);
 			break;
 		case 'l':
-			if(hero.move_animation==true) {
-				for(int col=0;col<height;col++) {
-					for(int row=0;row<width;row++) {
-						al_draw_bitmap_region(tileset, mv[col][row].sx * 16, mv[col][row].sy * 16, 16, 16, (row*16 - hero.wloc*16)+15*16+hero.frame, (col*16 - hero.hloc*16)+15*16, 0);
-					}
-				}	
-
-				if(hero.frame<=7) {
-					al_draw_bitmap_region(tileset, 1 * 16, 4 * 16, 16, 16, 15*16, 15*16, 0);
-				}
-				else if(hero.frame<=14) {
-					al_draw_bitmap_region(tileset, 2 * 16, 4 * 16, 16, 16, 15*16, 15*16, 0);
-				}
-				else if(hero.frame==15) {
-					al_draw_bitmap_region(tileset, 2 * 16, 4 * 16, 16, 16, 15*16, 15*16, 0);
-					hero.move_animation = false;
-					hero.wloc-=1;
-				}
-				hero.frame++;
-			}
-			else {
-				al_draw_bitmap_region(tileset, 0 * 16, 4 * 16, 16, 16, 15*16, 15*16, 0);
-			}
+			hero = walk_animation(hero,tileset,mv,tw,1,1,height,width,4,4,4,0,1,2);
 			break;
 		case 'r':
-			if(hero.move_animation==true) {
-				for(int col=0;col<height;col++) {
-					for(int row=0;row<width;row++) {
-						al_draw_bitmap_region(tileset, mv[col][row].sx * 16, mv[col][row].sy * 16, 16, 16, (row*16 - hero.wloc*16)+15*16-hero.frame, (col*16 - hero.hloc*16)+15*16, 0);
-					}
-				}	
-				if(hero.frame<=7) {
-					al_draw_bitmap_region(tileset, 1 * 16, 5 * 16, 16, 16, 15 * 16, 15 * 16, 0);
-				}
-				else if(hero.frame<=14) {
-					al_draw_bitmap_region(tileset, 2 * 16, 5 * 16, 16, 16, 15*16, 15*16, 0);
-				}
-				else if(hero.frame==15) {
-					al_draw_bitmap_region(tileset, 2 * 16, 5 * 16, 16, 16, 15*16, 15*16, 0);
-					hero.move_animation = false;
-					hero.wloc+=1;
-				}
-				hero.frame++;
-			}
-			else {
-				al_draw_bitmap_region(tileset, 0 * 16, 5 * 16, 16, 16, 15*16, 15*16, 0);
-			}
+			hero = walk_animation(hero,tileset,mv,tw,1,1,height,width,5,5,5,0,1,2);
 			break;
 		}
 	}
